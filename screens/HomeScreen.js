a34546d33623bbda64d9f14c0071e5878d7b6f15
Kornel Dubieniecki <lekterable@gmail.com>
2018-09-22 20:48:31 +0200

    Display player list

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, Button } from 'react-native'
+import { Text, Button, TextInput } from 'react-native'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 
@@ -11,18 +11,85 @@ const Container = styled.View`
 	justify-content: center;
 `
 
+const IDWrapper = styled.View`
+	display: flex;
+	flex-direction: column;
+	flex: 1;
+	align-items: center;
+	justify-content: center;
+`
+
+const IDLabel = styled.Text``
+
+const IDField = styled.Text`
+	text-align: center;
+	width: 200;
+	border: 1px grey solid;
+	padding: 5px;
+	border-radius: 5;
+	margin-top: 5;
+`
+
+const PlayerList = styled.FlatList`
+	margin-top: 5;
+	flex: 1;
+`
+
+const InviteButton = styled.Button``
+
+const Player = styled.Text`
+	text-align: center;
+	width: 200;
+	border: 1px grey solid;
+	padding: 5px;
+	border-radius: 5;
+`
+
 class HomeScreen extends React.Component {
+	state = {
+		players: [],
+		opponent: ''
+	}
+
+	componentDidMount() {
+		this.props.socket.emit('info')
+
+		this.props.socket.on('info', data => {
+			this.setState({
+				players: data.players.filter(player => player !== this.props.socket.id)
+			})
+		})
+	}
 	render() {
-		const { value, increase, decrease } = this.props
 		return (
 			<Container>
-				<Text>Home</Text>
+				<IDWrapper>
+					<IDLabel>My ID:</IDLabel>
+					<IDField>{this.props.socket.id}</IDField>
+				</IDWrapper>
+				<Text>Players:</Text>
+				<PlayerList
+					data={this.state.players}
+					renderItem={({ item }) => (
+						<Player onPress={() => this.setState({ opponent: item })}>
+							{item}
+						</Player>
+					)}
+					keyExtractor={(item, index) => index.toString()}
+				/>
+				<TextInput onChangeText={opponent => this.setState({ opponent })}>
+					{this.state.opponent}
+				</TextInput>
+				<InviteButton
+					title="Invite"
+					onPress={() => console.log(this.state.opponent)}
+				/>
 			</Container>
 		)
 	}
 }
 
 export default connect(
-	state => ({}),
+	state => ({ socket: state.socket }),
 	dispatch => ({})
 )(HomeScreen)
